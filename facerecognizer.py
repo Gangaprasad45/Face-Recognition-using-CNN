998527e8bfb9a2d36740621b96d2a2da5f0cb294
Gangaprasad Chinmod <61907816+Gangaprasad45@users.noreply.github.com>
2020-06-25 09:28:07 +0530

    Update facerecognizer.py

--- facerecognizer.py
+++ facerecognizer.py
@@ -14,16 +14,16 @@ from inception_blocks_v2 import *
 FR_model = load_model('nn4.small2.v1.h5')
 print("Total Params:", FR_model.count_params())
 
-face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+Face_detect = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 threshold = 0.25
 
-face_database = {}
+Image_database = {}
 
 for name in os.listdir('images'):
 	for image in os.listdir(os.path.join('images',name)):
 		identity = os.path.splitext(os.path.basename(image))[0]
-		face_database[identity] = fr_utils.img_path_to_encoding(os.path.join('images',name,image), FR_model)
+		Image_database[identity] = fr_utils.img_path_to_encoding(os.path.join('images',name,image), FR_model)
 
 print(face_database)
 
@@ -32,7 +32,7 @@ while True:
 	ret, frame = video_capture.read()
 	frame = cv2.flip(frame, 1)
 
-	faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+	faces = Face_detect.detectMultiScale(frame, 1.3, 5)
 	for(x,y,w,h) in faces:
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
 		roi = frame[y:y+h, x:x+w]
@@ -40,7 +40,7 @@ while True:
 		min_dist = 100
 		identity = None
 
-		for(name, encoded_image_name) in face_database.items():
+		for(name, encoded_image_name) in Image_database.items():
 			dist = np.linalg.norm(encoding - encoded_image_name)
 			if(dist < min_dist):
 				min_dist = dist
